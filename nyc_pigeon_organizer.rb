4d28e931332ec481b9afaa76b353bb8fc0e90a66
Jerry Huang <jhuang9240@gmail.com>
2019-10-31 14:26:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,8 @@ def nyc_pigeon_organizer(data)
 
     value.each do |quality, list|
       list.each do |name|
-        pigeons_of_nyc[name] = {} unless nil != pigeons_of_nyc[name]
-        pigeons_of_nyc[name][key] = [] unless nil != pigeons_of_nyc[name][key]
+        pigeons_of_nyc[name] = {} if nil = pigeons_of_nyc[name]
+        pigeons_of_nyc[name][key] = [] if nil = pigeons_of_nyc[name][key]
         pigeons_of_nyc[name][key].push(quality.to_s)
       end
     end
