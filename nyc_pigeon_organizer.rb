7c9101abde1699418a1e7b1f04354ffc640e9f07
Jerry Huang <jhuang9240@gmail.com>
2019-10-31 14:13:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,10 @@
 def nyc_pigeon_organizer(data)
-  pigeons_of_nyc = {{{}}}
+  pigeons_of_nyc = {}
   data.each do |key,value|
 
     value.each do |quality, list|
       list.each do |name|
+        
         pigeons_of_nyc[name][key] = quality
       end
     end
